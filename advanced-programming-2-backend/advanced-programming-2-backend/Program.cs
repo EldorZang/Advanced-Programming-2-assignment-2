97aa0d9ecf9cbe119e9ab5a1264e3bdcf5461c75
DorSror <Dor2419S@gmail.com>
2022-05-21 16:49:49 +0300

    removed user, comments from ratings - added star rating to creation view to do - display rating scores as stars, display ratings in list rather than table

---
+++ advanced-programming-2-backend/advanced-programming-2-backend/Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using advanced_programming_2_backend.Data;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<advanced_programming_2_backendContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("advanced_programming_2_backendContext") ?? throw new InvalidOperationException("Connection string 'advanced_programming_2_backendContext' not found.")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Ratings}/{action=Index}/{id?}");
+
+app.Run();
--- advanced-programmins-2-assignment-2-asp/advanced-programmins-2-assignment-2-asp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace advanced_programmins_2_assignment_2_asp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
