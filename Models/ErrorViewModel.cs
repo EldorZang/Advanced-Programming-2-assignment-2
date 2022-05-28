85b28451ff7fc6f40fed86a62e3e9f2a8086aaa4
EldorZang <92646424+EldorZang@users.noreply.github.com>
2022-05-28 23:09:55 +0300

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace advanced_programming_2_backend.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
